5b481ffd6b8be226930b80fb9a63a49d78d1fb90
WawanCoding18 <dzakwan112016@gmail.com>
2025-08-09 18:13:27 +0700

    setting type node for success axios

--- src/libs/axios/instance.ts
+++ src/libs/axios/instance.ts
@@ -1,41 +1,36 @@
-import environment from "@/config/environtment";
-import axios from "axios";
-import {Session} from "next-auth"
-import {getSession} from "next-auth/react"
+import environment from "@/config/environtment"
+import axios, { InternalAxiosRequestConfig } from "axios"
+import { Session } from "next-auth"
+import { getSession } from "next-auth/react"
 
-//For extends customsession to session for ensure accessToken value
 interface CustomSession extends Session {
-    accessToken?: string
+  accessToken?: string
 }
 
-//header content type
 const headers = {
-    "Content-Type": "application/json",
+  "Content-Type": "application/json",
 }
 
-//Make axios enviroment
 const instance = axios.create({
-    baseURL: environment.API_URL,
-    headers,
-    timeout: 60*1000
+  baseURL: environment.API_URL,
+  headers,
+  timeout: 60 * 1000,
 })
 
-//If when success request and fail request 
 instance.interceptors.request.use(
-    async(request)=>{
-        const Session: CustomSession | null = await getSession()
-        if(Session && Session.accessToken){
-            request.headers.Authorization = `Bearer ${Session.accessToken}`
-        }
-        return request
-    },
-    (error) => Promise.reject(error)
+  async (request: InternalAxiosRequestConfig) => {
+    const session = (await getSession()) as CustomSession | null
+    if (session?.accessToken) {
+      request.headers.Authorization = `Bearer ${session.accessToken}`
+    }
+    return request
+  },
+  (error) => Promise.reject(error)
 )
 
-//If when success response and fail response 
 instance.interceptors.response.use(
-    (response) => response,
-    (error) => Promise.reject(error)
+  (response) => response,
+  (error) => Promise.reject(error)
 )
 
-export default instance;
+export default instance
